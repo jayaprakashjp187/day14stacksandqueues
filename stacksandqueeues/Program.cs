1f4a93961b6b93ac9d8c4b1aa535394f015804df
jayaprakash <jayaprakashjp187@gmail.com>
2023-06-08 01:16:30 +0530

    values are pushing in a given order usingstackds

---
+++ stacksandqueeues/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace stacksandqueeues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Stack<int> s = new Stack<int>();
+            s.Push(70);
+            s.Push(30);
+            s.Push(56);
+            foreach(int i in s)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
